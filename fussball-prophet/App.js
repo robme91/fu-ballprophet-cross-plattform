d3a712dc1fbc1ca71cbcb74471e17176d6db6842
Robin Mehlitz <robin.mehlitz@beuth-hochschule.de>
2018-05-14 16:16:25 +0200

    Added navigator and navigation to gameday creation screen

--- fussball-prophet/App.js
+++ fussball-prophet/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import GameDayCreationScreen from './src/screens/GameDayCreationScreen'
 
-export default class App extends React.Component {
-   _onCreateGameday = () => {
-    Alert.alert(
-      'Neuer Spieltag bitte!!',
-      'You did it!',
-    );
-  }
-  
-  
-  render() {
+class HomeScreen extends React.Component{
+  render(){
     return (
       <View style={styles.container}>
         <Button
-            onPress={this._onCreateGameday}
+            onPress={() => this.props.navigation.navigate('GameDayCreation')}
             title="Neuer Spieltag"
             accessibilityLabel="Erstelle einen neuen Spieltag"
         />
       </View>
     );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    GameDayCreation: GameDayCreationScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
   }
 }
 
---
+++ fussball-prophet/src/screens/GameDayCreationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+
+export default class GameDayCreationScreen extends React.Component{
+  render(){
+    return (
+      <View >
+        <Text>Neuer Spieltag</Text>
+      </View>
+    );
+  }
+}
